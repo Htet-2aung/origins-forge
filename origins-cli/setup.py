3396f52a8f8b992e788a91f5c5db9d743182b7c4
Htet Aung <kienbrown212001@gmail.com>
2026-01-08 19:32:04 +0700

    Update setup.py
    Signed-off-by: Htet Aung <kienbrown212001@gmail.com>

--- origins-cli/setup.py
+++ origins-cli/setup.py
@@ -2,16 +2,17 @@ from setuptools import setup
 
 setup(
     name="origins-cli",
-    version="1.0",
+    version="3.0",
     py_modules=["main"],
     install_requires=[
         "typer[all]",
         "rich",
-        "shellingham",
+        "requests",
+        "openai",
     ],
     entry_points={
         "console_scripts": [
-            "origins=main:app",  # This maps the command 'origins' to your app
+            "origins=main:app", 
         ],
     },
 )
